f1475c7c689c6cf0078ca02fe3aba9429ecec30c
AnthonyJethro <AnthonyJethro@users.noreply.github.com>
2025-04-28 10:41:21 +0100

    Data and Initial Prompt.
    Resources that can be posted publicly.

---
+++ config.py
@@ -0,0 +1,26 @@
+
+# OpenAI API Key
+OPENAI_KEY = ""
+
+
+
+# Initial Prompt Category 1
+Prompt_Ethos = """Is the following text hate speech?"""
+Prompt_Liar = """Determine whether the statement is a lie (Yes) or not (No) based on the context and other information."""
+Prompt_Ar_Sarcasm = """Is this tweet sarcastic?"""
+# Initial Prompt Category 2
+Prompt_Tax_Product = """Is this text about a tax product that is taxed by the authority?"""
+Prompt_Tax_Transaction = """Is this text about a tax transaction?"""
+Prompt_Tax_Exemption = """Is this a tax exemption?"""
+Prompt_Tax_Calculation = """Is this a method or formula for calculating tax?"""
+Prompt_Tax_Subject = """Is this text about someone who is liable to pay or report tax?"""
+Prompt_Tax_Process = """Is this a process of paying tax to the authority?"""
+Prompt_Obligation = """Is this text an obligation?"""
+# Initial Prompt Category 3
+Prompt_Financial_Sentiment = """Is this text expressing a positive overall sentiment?"""
+Prompt_Amazon_Review = """Is this text a positive review?"""
+Prompt_Tweet_Airline = """Is this text containing a positive meaning?"""
+Prompt_Tweet_Hate_Speech = """Is this text hate speech?"""
+Prompt_Tweet_Offensive_Language = """ Is this text offensive language?"""
+Prompt_Clickbait = """Is this text clickbait?"""
+
